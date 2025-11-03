5c3736fb2e5fa222c369d223b28a6f767a766702
keon22han <keonheehan@hansung.ac.kr>
2025-11-03 21:08:28 +0900

    feat..? bug..? : Refresh Api 연동 부탁드립니다..
    Refresh Api 연동 부탁드립니다..

--- Projects/babyMoa/babyMoa/App/AppDelegate.swift
+++ Projects/babyMoa/babyMoa/App/AppDelegate.swift
@@ -8,7 +8,7 @@ import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        // UserDefault 데이터 삭제 필요 시
+//        // MARK: UserDefault 데이터 삭제 필요 시 주석 해제하세요. refresh token 로직 추가하면 이렇게 할 필요 없습니다.
 //        if let bundleID = Bundle.main.bundleIdentifier {
 //                UserDefaults.standard.removePersistentDomain(forName: bundleID)
 //                print("✅ UserDefaults cleared for \(bundleID)")
