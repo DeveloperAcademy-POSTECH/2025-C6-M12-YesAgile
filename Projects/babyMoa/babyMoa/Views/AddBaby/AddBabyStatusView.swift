30f3a493e14c84110f50875c937cb23658005779
Winnerkorea <trueater@naver.com>
2025-11-06 09:33:33 +0900

    Refactor:Profile

--- Projects/babyMoa/babyMoa/Views/AddBaby/AddBabyStatusView.swift
+++ Projects/babyMoa/babyMoa/Views/AddBaby/AddBabyStatusView.swift
@@ -20,7 +20,7 @@ struct AddBabyStatusView: View {
                     }
                 })
                 
-//                BabyProfileImageView(profileImage: $viewModel.profileImage, selectedPhotoItem: $viewModel.selectedPhotoItem)
+                BabyProfileImageView()
                 
                 VStack(spacing: 20){
                     
--- Projects/babyMoa/babyMoa/Views/Guardian/GuardianCodeView.swift
+++ Projects/babyMoa/babyMoa/Views/Guardian/GuardianCodeView.swift
@@ -25,7 +25,7 @@ struct GuardianCodeView: View {
                 })
                 
                 // TODO: ViewModel에서 실제 아기 프로필 이미지 가져오기
-//                BabyProfileImageView(profileImage: .constant(nil), selectedPhotoItem: .constant(nil))
+                BabyProfileImageView()
                 
                 VStack(alignment: .center, spacing: 11){
                     Text(viewModel.babyName)
