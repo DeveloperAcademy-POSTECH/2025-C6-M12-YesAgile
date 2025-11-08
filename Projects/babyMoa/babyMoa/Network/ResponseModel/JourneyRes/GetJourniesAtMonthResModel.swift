02be69fedd2356eba78e17a0bb87269d469f3012
Pherd <taehyeonine@gmail.com>
2025-11-08 18:24:15 +0900

    feat: GetJourniesAtMonthResModel 추가 및 수정으로 충돌 가능성
    GetJourniesAtMonthResModel 테드가 추가해주신 부분으로 쓸 예정입니다.

--- Projects/babyMoa/babyMoa/Network/ResponseModel/JourneyRes/GetJourniesAtMonthResModel.swift
+++ Projects/babyMoa/babyMoa/Network/ResponseModel/JourneyRes/GetJourniesAtMonthResModel.swift
@@ -4,10 +4,20 @@
 //
 //  Created by pherd on 11/1/25.
 // GET / array<object>
-struct GetJourniesAtMonthResModel: Decodable {
+
+import Foundation
+
+struct GetJourniesAtMonthResModel: ResponseModelable {
     var journeyImageUrl: String
     var latitude: Double
     var longitude: Double
     var date: String // "2025-11-01"
     var memo: String
+    // 테드가 추가해주신 부분. 앞으로 써야함.
+    func toDomain() async -> Journey {
+        let image = await ImageManager.shared.downloadImage(from: journeyImageUrl)
+        return Journey(journeyImage: image, latitude: latitude, longitude: longitude, date: DateFormatter.yyyyDashMMDashdd.date(from: date)!, memo: memo)
+    }
 }
+
+
