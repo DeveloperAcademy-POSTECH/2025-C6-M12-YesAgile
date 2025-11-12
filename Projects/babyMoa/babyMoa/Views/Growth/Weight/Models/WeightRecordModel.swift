387868332f6d9032f6da4dc8b3e82daa4f736a16
Winnerkorea <trueater@naver.com>
2025-11-12 23:32:50 +0900

    Make-WeightView

---
+++ Projects/babyMoa/babyMoa/Views/Growth/Weight/Models/WeightRecordModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeightRecordModel.swift
+//  BabyMoa
+//
+//  Created by Baba on 11/12/25.
+//
+
+import Foundation
---
+++ Projects/babyMoa/babyMoa/Views/Growth/Weight/WeightDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  WeightDetailView.swift
+//  BabyMoa
+//
+//  Created by Baba on 11/12/25.
+//
+
+import SwiftUI
+
+struct WeightDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeightDetailView()
+}
