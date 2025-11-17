0e6735483bf860578c12e2dca0d6b3169479a5d9
Winnerkorea <trueater@naver.com>
2025-11-17 23:54:24 +0900

    이미지 압축 50%설정완료

--- Projects/babyMoa/babyMoa/Utilities/ImageManager.swift
+++ Projects/babyMoa/babyMoa/Utilities/ImageManager.swift
@@ -14,7 +14,7 @@ final class ImageManager {
     private init() { }
     
     /// UIImage를 Base64 문자열로 인코딩
-    func encodeToBase64(_ image: UIImage, compressionQuality: CGFloat = 1.0) -> String? {
+    func encodeToBase64(_ image: UIImage, compressionQuality: CGFloat = 0.5) -> String? {
         // UIImage를 JPEG 형식으로 Data 변환
         guard let imageData = image.jpegData(compressionQuality: compressionQuality) else {
             print("Failed to convert UIImage to Data")
