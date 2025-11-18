7e8c36d94259d33309464a6ea2a4d80ab9011737
Winnerkorea <trueater@naver.com>
2025-11-18 17:17:28 +0900

    SignOut 기능 구현 완료함

--- Projects/babyMoa/babyMoa/Views/Baby/BabyMainView.swift
+++ Projects/babyMoa/babyMoa/Views/Baby/BabyMainView.swift
@@ -47,18 +47,9 @@ struct BabyMainView: View {
         
         .alert("로그아웃할까요?", isPresented: $viewModel.showSignOutAlert, actions: {
             Button("로그아웃", role: .destructive) {
-                
-                if viewModel.signOut() {
-                    // sessionManager.sessionState = .loggedOut 에서 로그아웃을 처리해야 되나 어떻게 해야 되나?
-                    // 서버와 이야기 해야 한다.
-                    print("로그인 버튼을 클릭했습니다.")
-                }
-                
+                viewModel.signOut()
             }
-            Button("취소", role: .cancel) {
-                
-            }
-            
+            Button("취소", role: .cancel) {}
         })
     }
 }
