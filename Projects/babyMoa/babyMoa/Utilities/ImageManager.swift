69850ebe308aeb1d39ea0893f40f5b447b61a120
Pherd <taehyeonine@gmail.com>
2025-11-18 20:49:46 +0900

    fix: ImageManageer UIImage 사이즈 1024로 리사이즈
    -ImageManageer UIImage 사이즈 1024로 리사이즈

--- Projects/babyMoa/babyMoa/Utilities/ImageManager.swift
+++ Projects/babyMoa/babyMoa/Utilities/ImageManager.swift
@@ -13,6 +13,39 @@ final class ImageManager {
     
     private init() { }
     
+    /// UIImage를 리사이즈 (최대 크기 제한)
+    /// - Parameters:
+    ///   - image: 원본 이미지
+    ///   - maxSize: 최대 가로/세로 크기 (기본: 1024)
+    /// - Returns: 리사이즈된 UIImage
+    /// - Note: 배터리 절약 및 메모리 최적화를 위해 1024x1024로 제한
+    func resizeImage(_ image: UIImage, maxSize: CGFloat = 1024) -> UIImage {
+        let originalSize = image.size
+        
+        // 이미 작으면 원본 반환
+        if originalSize.width <= maxSize && originalSize.height <= maxSize {
+            return image
+        }
+        
+        // 비율 유지하면서 리사이즈
+        let aspectRatio = originalSize.width / originalSize.height
+        var newSize: CGSize
+        
+        if originalSize.width > originalSize.height {
+            newSize = CGSize(width: maxSize, height: maxSize / aspectRatio)
+        } else {
+            newSize = CGSize(width: maxSize * aspectRatio, height: maxSize)
+        }
+        
+        // 리사이즈 수행
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: CGRect(origin: .zero, size: newSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resizedImage ?? image
+    }
+    
     /// UIImage를 Base64 문자열로 인코딩
     func encodeToBase64(_ image: UIImage, compressionQuality: CGFloat = 1.0) -> String? {
         // UIImage를 JPEG 형식으로 Data 변환
